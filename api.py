db83d14ea85a64fac7b3ea05955ea2930d1839a0
juareix <juarezmaciel45@gmail.com>
2025-07-12 13:53:31 -0400

    refactor: corrige erro de segurança na vulnerabilidade do token

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
 # api.py
 import requests
+import streamlit as st
 
-API_TOKEN = "51942df7d44e4191a84466cecfbc7bec"
-HEADERS = {"X-Auth-Token": API_TOKEN}
+HEADERS = {"X-Auth-Token": st.secrets["X_AUTH_TOKEN"]}
 
 def get_matches(competition_code="PL", season="2024"):
     url = f"https://api.football-data.org/v4/competitions/{competition_code}/matches?season={season}"
