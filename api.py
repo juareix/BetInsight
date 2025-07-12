5e5dbe97d6c382bbf5cff880f0cc8c9e4f8c3e82
juareix <juarezmaciel45@gmail.com>
2025-07-11 22:17:12 -0400

    feat: cria modulos de conex endpoint por competicao e times

--- api.py
+++ api.py
@@ -11,3 +11,22 @@ def get_matches(competition_code="PL", season="2024"):
         return response.json().get("matches", [])
     else:
         return []
+
+
+
+#Função para pegar os ultimso resultados dos times:
+def get_last_matches_by_team(team_id, limit=5):
+    url = f"https://api.football-data.org/v4/teams/{team_id}/matches?status=FINISHED&limit={limit}"
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code == 200:
+        return response.json().get("matches", [])
+    else:
+        return []
+
+#Função get pra busca por competição
+def get_teams_by_competition(competition_code):
+    url = f"https://api.football-data.org/v4/competitions/{competition_code}/teams"
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code == 200:
+        return response.json().get("teams", [])
+    return []
